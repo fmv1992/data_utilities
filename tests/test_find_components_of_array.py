2ddca60f73c62ed71cb5b073bf6dc2c2de1ec31a
monteiro <fmv1992@gmail.com>
2017-03-18 11:01:40 -0300

    added function for find_components_of_array

---
+++ tests/test_find_components_of_array.py
@@ -0,0 +1,108 @@
+"""Test pandas_utilities from this module."""
+
+
+# import pprint
+import unittest
+import pandas_utilities as pu
+import numpy as np
+from test_metaclass import TestMetaClass
+
+N = 1000
+l = 100
+c = 15
+
+
+class FindComponentsOfArray(unittest.TestCase, metaclass=TestMetaClass):
+
+    """Test class for function of pandas_utilities.
+
+    The interface should make easy to specify:
+        - N: the quantity of tests to be run.
+        - l: the length of the arrays.
+        - c: the number of columns to generate each y.
+
+    The tests should cover the case of integer and non integer composition of
+    arrays.
+
+    The tests should cover the case of arrays equal to the result and equal to
+    compositions of other columns.
+
+    The tests should cover the case where there is no possible composition.
+
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup attributes once."""
+        cls.N = N
+        cls.l = l
+        cls.c = c
+        cls.data = pu.statistical_distributions_dataframe(
+            rows=cls.l,
+            columns=cls.c)
+
+    def setUp(self):
+        """setUp method from unittest."""
+        pass
+
+    def gen_multipliers(self, n=N, amplitude=1500):
+        """Create a tuple of multipliers."""
+        MULT_AMPLITUDE = 1500
+        multipliers_pool = np.concatenate((
+            np.arange(1, MULT_AMPLITUDE + 1),
+            np.arange(- MULT_AMPLITUDE, -1 + 1)))
+
+        # Create all the multipliers once in memory.
+        multipliers = tuple(
+            np.random.choice(multipliers_pool, size=self.c)
+            * np.random.randint(0, 1+1, size=self.c, dtype=bool)
+            for x in range(N))
+
+        return multipliers
+
+    def gen_y_from_multipliers(self, multipliers):
+        """Create an iterator of y arrays from multipliers."""
+        mask = (multipliers != 0)
+
+        y = (self.data.iloc[:, mask] * multipliers[mask]).sum(axis=1)
+
+        return y
+
+    def dict_results_from_multipliers(self, multipliers):
+        """Create an iterator of dictionaries with answers from multipliers."""
+        mask = (multipliers != 0)
+        return dict(zip(self.data.columns[mask], multipliers[mask]))
+
+    def test_integer_composition(self):
+        """integer composition test."""
+        # Setup variables
+        multipliers = self.gen_multipliers()
+
+        # Map the multipliers to a generator of ys.
+        y_map = map(self.gen_y_from_multipliers, multipliers)
+
+        # Map the multipliers to a generator of dictionaries.
+        dicts_map = map(self.dict_results_from_multipliers, multipliers)
+
+        # Map the y's to a generator of find_components.
+        find_components_map = map(
+            lambda y: pu.find_components_of_array(self.data,
+                                                  y,
+                                                  assume_int=True),
+            y_map)
+
+        # Test.
+        self.assert_X_from_iterables(
+            self.assertEqual,
+            dicts_map,
+            find_components_map)
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(FindComponentsOfArray)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    # t = FindComponentsOfArray()
+    # print(t)
+    # pprint(t)
+    # pprint(dir(t))
+    # pass
---
+++ tests/test_metaclass.py
@@ -0,0 +1,77 @@
+"""This module provides support to actual tests.
+
+The unittest module in python is not so flexible to allow multiple tests with
+different inputs. Aggregating parameters in iterables and using a for loop does
+not seem to be elegant in this scenario.
+
+A different approach is to use meta classes to create instances with multiple
+tests.
+
+This latter approach will be tried here.
+
+A good tutorial for metaclasses:
+    https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses/
+
+The implementation of the idea to run parametrized tests in python comes from
+here:
+    http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python
+
+"""
+
+
+import unittest
+import inspect
+
+
+class TestMetaClass(type):
+
+    """Metaclass for all tests in this module.
+
+    This metaclass aims to facilitate iterable assertions (parametrized tests).
+
+    """
+
+    def __new__(mcs, name, bases, classdict):
+        """__new__ method of metaclass."""
+        def assert_X_from_iterables(self, x=lambda: True, *args):
+            func = getattr(self, x.__name__)
+            for i, zipargs in enumerate(zip(*args)):
+                # print('iteration i', i)
+                with self.subTest(iteration=i, arguments=zipargs):
+                    func(*zipargs)
+            return None
+        # METACLASS: create helper function.
+        classdict['assert_X_from_iterables'] = assert_X_from_iterables
+
+        # TODO: implement the verbosity attribute.
+        @property
+        def verbosity(cls):
+            """Return the verbosity setting of the currently running unittest.
+
+            This function 'scans' the
+
+            Returns:
+                int: the verbosity level.
+
+                0 if this is the __main__ file
+                1 if run with unittests module without verbosity (default in
+                TestProgram)
+                2 if run with unittests module with verbosity
+            """
+            frame = inspect.currentframe()
+            # Scans frames from innermost to outermost for a TestProgram
+            # instance.  This python object has a verbosity defined in it.
+            while frame:
+                cls = frame.f_locals.get('cls')
+                if isinstance(cls, unittest.TestProgram):
+                    return cls.verbosity
+                # Proceed to one outer frame.
+                frame = frame.f_back
+            return 0
+        # METACLASS: set verbosity.
+        classdict['verbosity'] = verbosity
+        return type.__new__(mcs, name, bases, classdict)
+
+
+if __name__ == '__main__':
+    unittest.main()
