e763ec7e66cfe2afaf4cbc609541e65b113abdea
Felipe M. Vieira <fmv1992@gmail.com>
2017-09-30 17:31:22 -0300

    added support for a BasePersistentGrid object

--- data_utilities/sklearn_utilities/grid_search.py
+++ data_utilities/sklearn_utilities/grid_search.py
@@ -11,6 +11,10 @@ import pickle
 import os
 import hashlib
 import io
+import zipfile
+import multiprocessing as mp
+
+import functools
 
 from data_utilities import sklearn_utilities as su
 
@@ -23,7 +27,7 @@ class BasePersistentGrid(object):
             >>> persistent_grid = (
                 grid_search.BasePersistentGrid.load_from_path(path))
         """
-        loaded_object = cls.load_from_path(*args, **kwargs)
+        loaded_object = cls.load_from_path(**kwargs)
         if loaded_object is None:  # Intialize with __init__ method.
             # Will call init and set all attributes (including data set's).
             return super(BasePersistentGrid, cls).__new__(cls)
@@ -35,8 +39,8 @@ class BasePersistentGrid(object):
                  dataset_path=None,
                  hash_function=hashlib.md5,
                  save_every_n_interations=1):
+
             # Create creation date if needed.
-            self.lock = None
             self.temporary_results = {}
 
             # These attributes are constant even between runs.
@@ -46,12 +50,20 @@ class BasePersistentGrid(object):
 
             # These two attributes can change between interactions.
             self.dataset_path = dataset_path
-            self.dataset_hash = (
-                self.dataset_hash if hasattr(self, 'dataset_hash')
-                else self._update_data_set_hash(self.dataset_path))
+            if not hasattr(self, 'dataset_hash'):
+                self._update_dataset_hash(self.dataset_path)
+
+            # Initilize multiprocessing attributes: manager, lock, data and
+            # _n_counter.
+            self._instantiate_shared_attributes()
+
+            # Zip file object to be written to disk from time to time.
+            # self._data_zip = zipfile.ZipFile(io.BytesIO(), mode='x')
+            # Unpacked dictionary: hash -> value
+            # self.data = self._load_data_from_bytesio()
 
             # These attributes change every session.
-            self._n_counter = 0
+            # self._n_counter = 0
 
     @classmethod
     def load_from_path(cls, *args, **kwargs):
@@ -63,37 +75,107 @@ class BasePersistentGrid(object):
         if os.path.isfile(path):
             with open(path, 'rb') as f:
                 loaded_object = pickle.load(f)
-            # Refresh data set hash. All other attributes are kept.
-            loaded_object._update_data_set_hash(kwargs['dataset_path'])
+            # Refresh data set hash.
+            loaded_object._update_dataset_hash(kwargs['dataset_path'])
+            # All other attributes are kept.
+            loaded_object._instantiate_shared_attributes()
             return loaded_object
         else:
             return None
 
-    def _update_data_set_hash(self, dataset_path):
-        return self.get_hash_from_path(dataset_path)
+    def get_multiprocessing_manager(self):
+        return self.mp_manager
 
-    def update(self, estimator, results):
-        # Get A = hash(hash(dataset) + hash(estimator)).
-        # Get B = hash(results).
-        # Store results in temporary_results[A][B].
-        # Store latest updated time.
-        # If needed write it to ./A/B.pickle.
+    def _instantiate_shared_attributes(self):
+        # Common manager (for other common attributes).
+        self.mp_manager = mp.Manager()
+        # Common lock.
+        self.mp_lock = self.mp_manager.Lock()
+        # Shared computed values (data).
+        self.mp_data = self.mp_manager.dict()
+        # Common attributes update counter (_n_counter).
+        self._mp_n_counter_value = self.mp_manager.Value(int, 0)
+
+    def _update_dataset_hash(self, dataset_path):
+        self.dataset_hash = self.get_hash(dataset_path)
+
+    def update(self, estimator, grid, results):
+        request_hash = self.compute_request_hash(estimator, grid)
+        with self.mp_lock:
+            # print('before dict', self.mp_data)
+            self.mp_data[request_hash] = results
+            # print('after dict', self.mp_data)
+            # print('before int', self._mp_n_counter_value)
+            self._mp_n_counter_value.value += 1
+            # print('after int', self._mp_n_counter_value)
+            if self._mp_n_counter_value.value % self.save_every_n_interations == 0:
+                self.save()
+
+    def _get_multiprocessing_shared_attributes(self):
         pass
+
+    def save(self):
+        _store_manager = self.mp_manager
+        del self.mp_manager
+        with open(self.persistence_grid_path, 'wb') as f:
+            pickle.dump(self, f)
+        self.mp_manager = _store_manager
+
+    def compute_request_hash(self, estimator, grid):
+        estimator_hash = self.get_hash(su.get_estimator_name(estimator))
+        grid_hash = self.get_hash(grid)
+        final_hash = self.get_hash(
+            self.dataset_hash + estimator_hash + grid_hash)
+        return final_hash
 
     def retrieve(self, estimator, grid):
         # If already stored just return.
-        # Else calculate, update and return.
-        pass
+        request_hash = self.compute_request_hash(estimator, grid)
+        return self.mp_data.get(request_hash, None)
 
-    def get_hash_from_path(self, path):
-        # Tested ;)
+    def get_hash(self, x):
+        # TODO: cached for strings and paths.
+        # Need to generalize the reading of:
+        #   1) file paths
+        #   2) small strings
+        #   3) python_objects
+        if isinstance(x, dict) or not os.path.isfile(x):  # use function cache.
+            return self._get_hash_from_hashable(self._transform_to_hashable(x))
+        # For files.
         hash_obj = self.hash_function()
-        with open(path, 'rb') as f:
-            data = f.read(io.DEFAULT_BUFFER_SIZE)
+        iter_of_bytes = open(x, 'rb')
+        try:
+            data = iter_of_bytes.read(io.DEFAULT_BUFFER_SIZE)
             while data:
                 hash_obj.update(data)
-                data = f.read(io.DEFAULT_BUFFER_SIZE)
+                data = iter_of_bytes.read(io.DEFAULT_BUFFER_SIZE)
+        finally:
+            iter_of_bytes.close()
         return hash_obj.digest()
+
+    def _transform_to_hashable(self, x):
+        if isinstance(x, str):
+            bytesobj = x.encode()
+        elif isinstance(x, bytes):
+            bytesobj = x
+        else:
+            bytesobj = pickle.dumps(x)
+        return bytesobj
+
+    @functools.lru_cache(maxsize=2**12)
+    def _get_hash_from_hashable(self, hashable):
+        hash_obj = self.hash_function()
+        hash_obj.update(hashable)
+        return hash_obj.digest()
+
+
+    def _load_data_from_bytesio(self, zipf):
+        """Return a dictionary of hexdigest -> results."""
+        keys = map(lambda x: x.strip('.pickle'), zipf.namelist())
+        values = map(zipf.read, zipf.namelist())
+        # TODO: use a shared dictionary
+        return dict(zip(keys, values))
+
 
 class PersistentGrid(BasePersistentGrid):
     """Enable easy persistence of calculated values for grid searchs.
@@ -120,8 +202,10 @@ class PersistentGrid(BasePersistentGrid):
         * A unique classifier tied to it.
             *
     """
+    pass
 
 if __name__ == '__main__':
     bpg = PersistentGrid(
         '../../__/persistent_grid.pickle',
         '../../__/iris_dataset.csv')
+    import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
--- data_utilities/tests/test_sklearn_utilities.py
+++ data_utilities/tests/test_sklearn_utilities.py
@@ -1,9 +1,12 @@
 """Test sklearn_utilities from this module."""
 
+import os
 import datetime as dt
 import multiprocessing
 
+import pandas as pd
 import numpy as np
+
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
@@ -24,32 +27,80 @@ class TestGridSearchCV(TestDataUtilitiesTestCase, metaclass=TestMetaClass):
 
     data_ml_x, data_ml_y = datasets.make_hastie_10_2(
         n_samples=60000, random_state=1)
+    small_grid = {'n_estimators': list(range(1, 3)),
+                  'max_depth': [2, 4, 6],
+                  'min_samples_leaf': [.2],
+                  'n_jobs': [1, ],
+                  }
 
-    def test_multiparallelism_speed(self):
-        """Test that using more processes speed up grid search."""
-        clf = RandomForestClassifier()
-        random_forest_grid = {
-            'n_estimators': list(range(1, 3)),
-            'max_depth': [2, 4, 6],
-            'min_samples_leaf': [.2],
-        }
+    @classmethod
+    def setUpClass(cls):
+        # Call parent class super.
+        super(TestGridSearchCV, cls).setUpClass()
+        # Create support directories.
+        cls.temp_directory_grid_search = os.path.join(cls.temp_directory.name,
+                                                      'test_grid_search_cv')
+        os.mkdir(cls.temp_directory_grid_search)
+        cls.temp_directory_grid_search_data = os.path.join(
+            cls.temp_directory_grid_search, 'data')
+        os.mkdir(cls.temp_directory_grid_search_data)
+        # Save data.
+        cls.csv_path = os.path.join(cls.temp_directory_grid_search_data,
+                                    'data.csv')
+        pd.concat(map(pd.DataFrame,
+                      (cls.data_ml_x, cls.data_ml_y)),
+                  axis=1).to_csv(cls.csv_path)
 
-        all_times = []
-        N_RUNS = 3
-        for processors in (1, multiprocessing.cpu_count()):
-            processor_times = []
-            for _ in range(N_RUNS):
-                processor_times.append(
-                    time_function_call(
-                        su.grid_search_cv,
-                        random_forest_grid,
-                        clf,
-                        self.data_ml_x,
-                        n_jobs=processors,
-                        y=self.data_ml_y,
-                        persistence_path=None,
-                        scoring='roc_auc',
-                        cv=5))
-            all_times.append(np.mean(processor_times))
-        all_times = np.array(all_times)
-        assert all(np.diff(all_times) < 0)
+
+
+
+    # def test_multiparallelism_speed(self):
+    #     """Test that using more processes speed up grid search."""
+    #     clf = RandomForestClassifier()
+    #     random_forest_grid = {
+    #         'n_estimators': list(range(1, 3)),
+    #         'max_depth': [2, 4, 6],
+    #         'min_samples_leaf': [.2],
+    #     }
+
+    #     all_times = []
+    #     N_RUNS = 3
+    #     for processors in (1, multiprocessing.cpu_count()):
+    #         processor_times = []
+    #         for _ in range(N_RUNS):
+    #             processor_times.append(
+    #                 time_function_call(
+    #                     su.grid_search_cv,
+    #                     random_forest_grid,
+    #                     clf,
+    #                     self.data_ml_x,
+    #                     n_jobs=processors,
+    #                     y=self.data_ml_y,
+    #                     persistence_path=None,
+    #                     scoring='roc_auc',
+    #                     cv=5))
+    #         all_times.append(np.mean(processor_times))
+    #     all_times = np.array(all_times)
+    #     assert all(np.diff(all_times) < 0)
+    def test_grid_search(self):
+        persistent_grid_obj = su.grid_search.BasePersistentGrid(
+            persistence_grid_path=os.path.join(self.temp_directory_grid_search,
+                                               'bpg.pickle'),
+            dataset_path=self.csv_path)
+
+        # Do a first run.
+        grid = su.grid_search_cv(
+            persistent_grid_obj,
+            self.small_grid,
+            RandomForestClassifier(),
+            self.data_ml_x,
+            y=self.data_ml_y,
+            n_jobs=1)
+        import pprint
+        pprint.pprint(persistent_grid_obj)
+        pprint.pprint(dict(persistent_grid_obj.mp_data))
+        os.system('tree '+ self.temp_directory.name)
+        p2 = su.grid_search.BasePersistentGrid(
+            persistence_grid_path=os.path.join(self.temp_directory_grid_search,
+                                               'bpg.pickle'),
+            dataset_path=self.csv_path)
