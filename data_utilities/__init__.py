3c4b48935487b06a27339c15ab89f6826f541895
Felipe M. Vieira <fmv1992@gmail.com>
2019-10-12 17:02:45 -0300

    bumped to version 1.2.10

--- data_utilities/__init__.py
+++ data_utilities/__init__.py
@@ -61,7 +61,7 @@ import numpy as np
 # import data_utilities.tests.test_support as ts
 # from data_utilities.tests.test_support import TestDataUtilitiesTestCase
 
-__version__ = '1.2.9'
+__version__ = '1.2.10'
 
 # pylama: ignore=D406,D407
 
